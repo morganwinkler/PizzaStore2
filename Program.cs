8030bb85253d3cad09da0e24b485eeb311c404c0
Morgan Winkler <winkler.morgan1553@gmail.com>
2024-02-02 16:11:07 -0500

    Add CRUD

--- Program.cs
+++ Program.cs
@@ -24,8 +24,39 @@ app.UseSwaggerUI(c =>
   c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
 });
 
-app.MapGet("/", () => "Hello World!");
 
+// get ALL
 app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+// post CREATE
+app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
+{
+  await db.Pizzas.AddAsync(pizza);
+  await db.SaveChangesAsync();
+  return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
+// get by ID
+app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
+// put UPDATE
+app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatepizza, int id) =>
+{
+  var pizza = await db.Pizzas.FindAsync(id);
+  if (pizza is null) return Results.NotFound();
+  pizza.Name = updatepizza.Name;
+  pizza.Description = updatepizza.Description;
+  await db.SaveChangesAsync();
+  return Results.NoContent();
+});
+// DELETE
+app.MapDelete("/pizza/{id}", async (PizzaDb db, int id) =>
+{
+  var pizza = await db.Pizzas.FindAsync(id);
+  if (pizza is null)
+  {
+    return Results.NotFound();
+  }
+  db.Pizzas.Remove(pizza);
+  await db.SaveChangesAsync();
+  return Results.Ok();
+});
 
 app.Run();
